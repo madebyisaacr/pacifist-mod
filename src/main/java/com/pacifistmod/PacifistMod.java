2ff3ca086c81cb61357ee857d444425a036a26a2
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2026-03-08 13:33:31 -0400

    Initial commit

---
+++ src/main/java/com/pacifistmod/PacifistMod.java
@@ -0,0 +1,11 @@
+package com.pacifistmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PacifistMod implements ModInitializer {
+	public static final String MOD_ID = "pacifistmod";
+
+	@Override
+	public void onInitialize() {
+	}
+}
